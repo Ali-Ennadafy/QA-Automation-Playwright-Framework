c994110d9bcf0c0b91a612dbafc947ae62f94054
ali ennadafy <aennadafy@gmail.com>
2026-09-26 19:36:19 +0000

    push the ProductPage

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -1,17 +1,32 @@
 export class ProductPage {
-
     constructor(page) {
         this.page = page;
-        this.productNameInput = page.getByLabel('Product Name');
-        this.productDescriptionInput = page.getByLabel('Product Description');
-        this.productPriceInput = page.getByLabel('Product Price');
-        this.addToCartButton = page.getByRole('button', { name: 'Add to Cart' });
+        this.addToCartButton = page.getByRole('button', { name: 'Add to cart' });
+        this.quantityInput = page.locator('.qty-input');
+        this.successNotification = page.locator('.bar-notification.success');
     }
 
-    async addProductToCart(productName, productDescription, productPrice) {
-        await this.productNameInput.fill(productName);
-        await this.productDescriptionInput.fill(productDescription);
-        await this.productPriceInput.fill(productPrice);
+    async selectDropdownOption(labelName, optionText) {
+        await this.page.getByLabel(labelName).selectOption({ label: optionText });
+    }
+    async checkOption(optionLabel) {
+        await this.page.getByLabel(optionLabel).check();
+    }
+
+    async fillCustomTextInput(labelName, textValue) {
+        await this.page.getByLabel(labelName).fill(textValue);
+    }
+
+    async uploadProductFile(labelName, filePath) {
+        await this.page.getByLabel(labelName).setInputFiles(filePath);
+    }
+
+    async setQuantity(qty) {
+        await this.quantityInput.clear();
+        await this.quantityInput.fill(qty);
+    }
+    async clickAddToCart() {
         await this.addToCartButton.click();
     }
 }
+
